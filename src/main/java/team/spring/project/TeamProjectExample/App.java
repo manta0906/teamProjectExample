12149b5d3fde860c27a00120c652dc791bef664c
준현의LAPTOP\jun10 <jun10277@naver.com>
2021-08-16 22:28:06 +0900

    project init commit

---
+++ src/main/java/team/spring/project/TeamProjectExample/App.java
@@ -0,0 +1,13 @@
+package team.spring.project.TeamProjectExample;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
