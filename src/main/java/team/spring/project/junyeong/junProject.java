11335f1282227af7fe10bc89d60b7a76fa179064
이성호 <dltjdgh228@gmail.com>
2021-09-01 11:45:20 +0900

    자바파일 바꾸기

--- src/main/java/team/spring/project/junyeong/junProject.java
+++ src/main/java/team/spring/project/junyeong/junProject.java
@@ -3,8 +3,8 @@ package team.spring.project.junyeong;
 public class junProject {
 
 	public static void main(String[] args) {
-		for(int i=0; i<5; i++) {
-			System.out.println("안녕하세요");
+		for(int i=0; i<10; i++) {
+			System.out.println("i'm bugman");
 		
 		}
 
