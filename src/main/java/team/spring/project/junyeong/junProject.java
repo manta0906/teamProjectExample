7a3a1c3cd08701c61482d0039ffe78401b21a1bf
이성호 <dltjdgh228@gmail.com>
2021-09-01 13:49:49 +0900

    커밋

--- src/main/java/team/spring/project/junyeong/junProject.java
+++ src/main/java/team/spring/project/junyeong/junProject.java
@@ -3,8 +3,8 @@ package team.spring.project.junyeong;
 public class junProject {
 
 	public static void main(String[] args) {
-		for(int i=0; i<10; i++) {
-			System.out.println("i'm bugman");
+		for(int i=0; i<=11; i++) {
+			System.out.println("i'm seongho");
 		
 		}
 
