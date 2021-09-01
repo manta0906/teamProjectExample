d5979aded73e2f688f1e712b95dc0ed709fb44a0
준현의LAPTOP\jun10 <jun10277@naver.com>
2021-09-01 09:47:52 +0900

    test12

---
+++ src/main/java/team/spring/project/TeamProjectExample/Test102.java
@@ -0,0 +1,11 @@
+package team.spring.project.TeamProjectExample;
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "테스트1" );
+        System.out.println("테스트2");
+    }
+}
