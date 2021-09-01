568346a8c248055fc5a435331cd0a4a41d77fa18
김단아 <kimdanah93@gmail.com>
2021-09-01 13:08:48 +0900

    내용 수정

--- src/main/java/team/spring/project/junyeong/junProject.java
+++ src/main/java/team/spring/project/junyeong/junProject.java
@@ -5,9 +5,8 @@ public class junProject {
 	public static void main(String[] args) {
 		for(int i=0; i<10; i++) {
 			System.out.println("i'm bugman");
-		
 		}
-
+		System.out.println("yeah, you're a bugman");
 	}
 
 }
