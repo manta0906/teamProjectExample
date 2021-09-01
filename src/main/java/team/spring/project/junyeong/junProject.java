5fb4af94cc5586718d884adf24e4d92b7fbf5445
오준영 <ojy0533@naver.com>
2021-09-01 13:49:49 +0900

    커밋

--- src/main/java/team/spring/project/junyeong/junProject.java
+++ src/main/java/team/spring/project/junyeong/junProject.java
@@ -4,7 +4,7 @@ public class junProject {
 
 	public static void main(String[] args) {
 		for(int i=0; i<10; i++) {
-			System.out.println("i'm bugman");
+			System.out.println("i'm bugwoman");
 		
 		}
 
